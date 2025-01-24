798b5e9d376c3522716a2fdb3811cc46bdf68d44
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-24 11:53:36 +0530

    updated

--- events/form.js
+++ events/form.js
@@ -1,3 +1,13 @@
 $(document).ready(()=>{
     console.log("Radhe Radhe");
+    $("#exampleInputEmail1,#exampleInputPassword1").focus(function(){
+        $(this).css("background","lightblue");
+    })
+    $("#exampleInputEmail1,#exampleInputPassword1").blur(function(){
+        $(this).css("background","");
+    })
+    $("#exampleFormControlSelect1").change(function(){
+        $(this).css("background","pink");
+        $("#myp").html($(this).val());
+    })
 })
