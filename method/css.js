23276a7427b2de5df6014e7051fb4b30149a0fe7
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-27 14:22:09 +0530

    updated

---
+++ method/css.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    console.log("Radhe Radhe");
+    $('#mybtn').click(function(){
+        $('#box').css("background-color","#87cefa"); 
+        $('#mypara').text("Whenever there is a decline in righteousness and an increase in unrighteousness, O Arjuna, at that time I manifest Myself on earth.");
+    })
+})
