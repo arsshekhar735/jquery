5454598dc7f128a1ca694fbe1be16802cb763717
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-27 14:41:36 +0530

    updated

--- method/css.js
+++ method/css.js
@@ -3,5 +3,16 @@ $(document).ready(function(){
     $('#mybtn').click(function(){
         $('#box').css("background-color","#87cefa"); 
         $('#mypara').text("Whenever there is a decline in righteousness and an increase in unrighteousness, O Arjuna, at that time I manifest Myself on earth.");
+        $('#mybtn').text("You have clicked");
+    })
+    $('#dark').click(function(){
+        $('#box #mypara').css({
+            
+            "color":"#ffffff"
+        })
+        $('#box').css({
+            "background-color":"black",
+           
+        })
     })
 })
