ae963f8d1b4efe63ed1f9304b38a8f2a98dae424
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-27 11:35:41 +0530

    updated

--- events/set_method.js
+++ events/set_method.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     $("#mybtn").click(function(){
         $('#box .h').html("<strong>Hello Radhe Radhe</strong>")
         $('#mypara').text("Your right is to perform your duty only, but never to its fruits. Let not the fruits of action be your motive, nor let your attachment be to inaction.")
+        $('#mypara').attr('class','pink');
+        $('#sname').val("Suman Shekhar");
+        $('#sclass').val("8th Semester");
     })
     
 })
