5193b5926807c3e2b12831a62c333f515ffcb4fd
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-28 12:31:23 +0530

    updated

--- method/append&prepend/append&prepend.js
+++ method/append&prepend/append&prepend.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     //Append
     $('#btn').click(function(){
-        $('#mypara').append("Weapons cannot cut the soul, nor can fire burn it.Water cannot wet it, nor can the wind dry it.")
+        console.log("Radhe Radhe")
+        $('#mypara').append("<br>Weapons cannot cut the soul, nor can fire burn it.Water cannot wet it, nor can the wind dry it.")
+        console.log("Jai Shree ram")
     })
 })
