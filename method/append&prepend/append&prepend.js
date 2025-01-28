0c26556754557bc3c547bd307edfe8f0332b78ce
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-28 12:07:11 +0530

    updated

--- method/append&prepend/append&prepend.js
+++ method/append&prepend/append&prepend.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    
+    //Append
+    $('#btn').click(function(){
+        $('#mypara').append("Weapons cannot cut the soul, nor can fire burn it.Water cannot wet it, nor can the wind dry it.")
+    })
 })
