15b472dabc73ae157da296ab4960375b0f68baa1
Suman Shekhar <suman.nwd03@gmail.com>
2025-01-28 12:44:29 +0530

    updated

--- method/append&prepend/append&prepend.js
+++ method/append&prepend/append&prepend.js
@@ -5,4 +5,7 @@ $(document).ready(function(){
         $('#mypara').append("<br>Weapons cannot cut the soul, nor can fire burn it.Water cannot wet it, nor can the wind dry it.")
         console.log("Jai Shree ram")
     })
+    $('#btn1').click(function(){
+        $('#mypara').prepend("<h3>Verse 2.19</h3>");
+    })
 })
